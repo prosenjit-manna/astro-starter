e4708bf4c2ecc7001ee7ae9dd6f50eb1b666fef2
Prosenjit Manna <prosenjit@itobuz.com>
2025-10-23 10:07:24 +0530

    react integration added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  devToolbar: {
+      enabled: false
+  },
+
+  integrations: [react()]
+});
